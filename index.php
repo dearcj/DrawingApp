0861bdfc9882aa1c575610961f154bb16f062c20
Alexey <varenicza@gmail.com>
2016-04-09 19:26:31 +0300

    Переверстали экран рисования под нереспонсив

--- index.php
+++ index.php
@@ -182,7 +182,7 @@
         <div id="filters-wrapper" class="hidden">
           <div class="filters-canvas">
 
-            <canvas id="filters-canvas"></canvas>
+            <img id="filters-canvas">
             <div class="dark-side filters"></div>
 
             <ul class="tools-buttons filters-tb">
