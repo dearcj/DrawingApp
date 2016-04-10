2120039a416ab4a92a4e4bd85f5345ef11facd20
KURWINDALLAS <ddearcj@gmail.com>
2016-04-10 23:15:45 +0300

    api fix

--- api/test.php
+++ api/test.php
@@ -5,4 +5,6 @@
 		    $image = 'data:image/jpeg;base64,/9j/4AAQSkZJRgABAQEAWgBaAAD/4gxYSUNDX1BST0ZJTEUAAQEAAAxITGlubw';
 			$image = preg_replace('#^data:image/[^;]+;base64,#', '', $image);
 			$data = base64_decode($image);
-			file_put_contents($imgfilename . '.jpg', $data);
+			echo $data;
+
+			file_put_contents('/imgs/' . $imgfilename . '.jpg', $data);
