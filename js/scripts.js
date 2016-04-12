c8c332e6a79b68649492cd256df33a16f9516f85
Alexey <varenicza@gmail.com>
2016-04-13 00:24:11 +0300

    Routing

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ jQuery(document).ready(function($) {
 	var stateObj = { home: "screen" };
 
 	function addURL(pageName) {
-		var newHref = window.location.href + 'index.php?p=' + pageName;
+		var newHref = window.location.href + '?p=' + pageName;
 		history.replaceState(stateObj, pageName, newHref);
 	}
 
