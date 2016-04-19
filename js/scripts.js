b1c25eaf0507104536db203a555ec7f344355497
Alexey <varenicza@gmail.com>
2016-04-20 01:09:51 +0300

    routing

--- js/scripts.js
+++ js/scripts.js
@@ -68,6 +68,7 @@ jQuery(document).ready(function($) {
 	});
 
 	window.addEventListener("popstate", function(e) {
+		console.log(window.prevHref);
 		if(window.prevHref.indexOf(surface) > 0) {
 			openPage('surface', 'index', 'painting');
 		}
