62686e984185206c25ba16ca0c8681b3481f6301
Alexey <varenicza@gmail.com>
2016-04-20 02:11:07 +0300

    routing

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,7 @@ jQuery(document).ready(function($) {
 
 	function addURL(pageName) {
 		var newHref = mainUrl + '?p=' + pageName;
-		var currentHref = window.currentState;
+		window.currentHref = window.currentState;
 		history.replaceState(stateObj, pageName, newHref);
 		console.log(currentHref);
 	}
