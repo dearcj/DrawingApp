b72b8210c2443ef865397c503362c2f535e78aeb
Alexey <varenicza@gmail.com>
2016-04-20 22:09:09 +0300

    routing

--- js/scripts.js
+++ js/scripts.js
@@ -69,7 +69,7 @@ jQuery(document).ready(function($) {
 	$('.go-to-publication').click(function() {
 		window.currentState = window.location.href;
 		openPage('publication', 'painting', 'filters');
-		for (i=1; i<=3: i++) {
+		for (var i=1; i<=3: i++) {
 			var randomIntLeft = getRandomInt(1, 12);
 			var randomIntRight = getRandomInt(1, 12);
 			$('ul.paintings.left-side a:nth-child(' + i + ') li').css('background', 'url(../imgs/painting-' + randomIntLeft + '.jpg) no-repeat');
