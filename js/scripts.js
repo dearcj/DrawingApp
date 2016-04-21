cf190e0ee8f9793d2558b799d4b6867f0db1d9d6
Alexey <varenicza@gmail.com>
2016-04-21 07:14:08 +0300

    routing

--- js/scripts.js
+++ js/scripts.js
@@ -145,6 +145,7 @@ jQuery(document).ready(function($) {
 		for (var j = 1; j < 4; j++) {
 			var randomInt = Math.floor(Math.random()*imgNumbers.length);
 			imgNumbers.splice(randomInt,1);
+			console.log(randomInt);
 			console.log(imgNumbers[randomInt]);
 			console.log(imgNumbers);
 
