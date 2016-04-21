4d7eaadc78f0555f8a02ec46addc2e6bb0949238
Alexey <varenicza@gmail.com>
2016-04-21 07:15:57 +0300

    routing

--- js/scripts.js
+++ js/scripts.js
@@ -144,10 +144,10 @@ jQuery(document).ready(function($) {
 
 		for (var j = 1; j < 4; j++) {
 			var randomInt = Math.floor(Math.random()*imgNumbers.length);
-			imgNumbers.splice(randomInt,1);
 			console.log(randomInt);
 			console.log(imgNumbers[randomInt]);
 			console.log(imgNumbers);
+			imgNumbers.splice(randomInt,1);
 
 			$('ul.paintings.left-side a:nth-child(' + j + ') li').css('background', 'linear-gradient(0deg, rgba(0,0,0,0), rgba(0,0,0,0)), url(' + imgPath + 'painting-' + imgNumbers[randomInt] + '.jpg) no-repeat');
 			$('ul.paintings.left-side a:nth-child(' + j + '):hover li').css('background', 'linear-gradient(0deg, rgba(0,0,0,0.8), rgba(0,0,0,0.8)), url(' + imgPath + 'painting-' + imgNumbers[randomInt] + '.jpg) no-repeat');
