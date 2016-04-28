1e25fed1a432b531cfa678409d846c334dd9a73e
Alexey <varenicza@gmail.com>
2016-04-28 18:46:16 +0300

    routing

--- js/scripts.js
+++ js/scripts.js
@@ -28,7 +28,8 @@ jQuery(document).ready(function($) {
 		$('#surface-canvas').css({width: 285 + 'px', height: 196 + 'px'});
 		$('#surface-canvas p').css('display', 'none');
 		$('.surface-button.at-the-backyard').css('display', 'none');
-		$('ul.surfaces').css({width: 150 + 'px', height: 220 + 'px', 'margin-top': 35 + 'px', 'margin-left': 0});
+		$('ul#helpers li').css('display', 'none');
+		$('ul.surfaces').css({width: 150 + 'px', height: 220 + 'px', 'margin-top': 35 + 'px', 'margin-left': 0 + '!important'});
 		$('ul.surfaces li').css({width: 75 + 'px', height: 50 + 'px', 'margin-top': 0 + 'px'});
 		$('ul.surfaces li:nth-child(2n+2)').css('margin-left', -8 + 'px');
 		$('ul.surfaces li.metal').css('margin-left', 35 + 'px');
