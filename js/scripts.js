ba08e483bd9f2cd9ee88197b58e239d4826989ad
Alexey <varenicza@gmail.com>
2016-05-04 06:40:32 +0300

    routing

--- js/scripts.js
+++ js/scripts.js
@@ -524,7 +524,10 @@ jQuery(document).ready(function($) {
 
 		openPage('publication');
 
-
+		if ($('#in-label').val() != '' && $('#in-label').val() != 'Напишите название работы') {
+			var test = $('#in-label').val();
+			console.log(test);
+		}
 
 		$('#wand').animate({'margin-left': -1000 + 'px'}, 500);
 		$('#painting-wrapper').delay(500).animate({'opacity': 1}, 100).css('z-index', '100');
