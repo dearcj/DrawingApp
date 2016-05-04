2e69953036dab51375934ae07508c6565c7125da
Alexey <varenicza@gmail.com>
2016-05-04 08:14:52 +0300

    routing

--- js/scripts.js
+++ js/scripts.js
@@ -341,13 +341,13 @@ jQuery(document).ready(function($) {
 				'opacity': 1,
 				'height': 100 + '%'
 			}, 200).css('z-index', '200');
-			$('ul.surfaces').css({width: surfaceWidth*2 + 'px', height: surfaceHeight*4 + 'px'}).delay(500).animate({'margin-left': ($(window).width() - 285)/4 - surfaceWidth + 'px', 'margin-top': (196 - surfaceHeight*4)/2 + 35 + 'px'}, 1500);
+			$('ul.surfaces').css({width: surfaceWidth*2 + 'px', height: surfaceHeight*4 + 'px'}).delay(500).animate({'margin-left': ($(window).width() - 285) /3.8 - surfaceWidth + 'px', 'margin-top': (196 - surfaceHeight*4)/2 + 35 + 'px'}, 1500);
 		} else {
 			$('#surface-wrapper').fadeIn(500).removeClass('hidden').animate({
 				'opacity': 1,
 				'height': 660 + 'px'
 			}, 200).css('z-index', '200');
-			$('ul.surfaces').css({width: surfaceWidth*2 + 'px', height: surfaceHeight*4 + 'px'}).delay(500).animate({'margin-left': ($(window).width() - 700)/4 - surfaceWidth + 'px', 'margin-top': (495 - surfaceHeight*4)/2 + 35 + 'px'}, 1500);
+			$('ul.surfaces').css({width: surfaceWidth*2 + 'px', height: surfaceHeight*4 + 'px'}).delay(500).animate({'margin-left': ($(window).width() - 700)/3.8 - surfaceWidth + 'px', 'margin-top': (495 - surfaceHeight*4)/2 + 35 + 'px'}, 1500);
 		}
 
 		$('#surface-wrapper').each(function () {
@@ -371,7 +371,7 @@ jQuery(document).ready(function($) {
 	}
 
 	function paintingAnimation() {
-		$('ul.surfaces').animate({'margin-left': -$(window).width()/5.4 + 'px'}, 500);
+		$('ul.surfaces').animate({'margin-left': -$(window).width()/5.28 + 'px'}, 500);
 		$('.go-to-tools').animate({'top': forwardButtonHeight + 'px'}, 500);
 		$('#surface-wrapper').delay(500).animate({'opacity': 0}, 100).css('z-index', '100');
 		$('#filters-wrapper').delay(500).animate({'opacity': 0}, 100).css('z-index', '100');
