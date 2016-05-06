bdef99f55e7b5356334ce6149409b4e3fa9d6de2
Alexey <varenicza@gmail.com>
2016-05-06 14:57:25 +0300

    routing

--- js/scripts.js
+++ js/scripts.js
@@ -58,9 +58,9 @@ jQuery(document).ready(function($) {
 
 		$('ul.tools-buttons li').css({width: 65 + 'px', height: 65 + 'px', 'margin-top': 15 + 'px'});
 		$('ul.tools-buttons li p').css({'font-size': 8 + 'pt', bottom: 20 + 'px'});
-		$('#back-canvas').css({width: 240 + 'px', height: 165 + 'px', 'margin-left': -80 + 'px'});
-		$('#main-canvas').css({width: 240 + 'px', height: 165 + 'px', 'margin-left': -80 + 'px'});
-		$('#cursor-canvas').css({width: 240 + 'px', height: 165 + 'px', 'margin-left': -80 + 'px'});
+		$('#back-canvas').css({width: 240 + 'px', height: 165 + 'px', 'margin-left': -80 + 'px', top: 0});
+		$('#main-canvas').css({width: 240 + 'px', height: 165 + 'px', 'margin-left': -80 + 'px', top: 0});
+		$('#cursor-canvas').css({width: 240 + 'px', height: 165 + 'px', 'margin-left': -80 + 'px', top: 0});
 		$('ul.tools').css({width: 144 + 'px', height: 288 + 'px', top: 0});
 		$('ul.tools > li').css({width: 72 + 'px', height: 72 + 'px'});
 		$('ul.tools > li#coal > ul').css({width: 50 + 'px', height: 80 + 'px'});
@@ -81,7 +81,7 @@ jQuery(document).ready(function($) {
 		$('ul.tools li.needle-color').css({width: 15 + 'px', height: 15 + 'px', 'margin-left': -2 + 'px'});
 		$('ul.tools li#btn-pistol').css({width: 56 + 'px', height: 84 + 'px', 'margin-right': 2 + 'px'});
 		$('li.send-to-museum').css('width', 150 + 'px');
-		$('ul.tools-buttons.tools-tb').css({width: 232 + 'px', top: 225 + 'px', 'margin-left': -90 + 'px'});
+		$('ul.tools-buttons.tools-tb').css({width: 232 + 'px', top: 165 + 'px', 'margin-left': -91 + 'px'});
 
 		$('.filters-canvas').css({width: 240 + 'px', height: 165 + 'px'});
 		$('#filters-canvas').css({width: 240 + 'px', height: 165 + 'px'});
