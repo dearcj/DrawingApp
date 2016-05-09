0d719dccec0141270af20eeb6352645bb412c7f2
Alexey <varenicza@gmail.com>
2016-05-09 08:44:23 +0300

    routing

--- js/scripts.js
+++ js/scripts.js
@@ -341,12 +341,6 @@ jQuery(document).ready(function($) {
 			$('ul.paintings.left-side a:nth-child(' + (i + 1) + ') span.materials').text(leftImgMats);
 			$('ul.paintings.left-side a:nth-child(' + (i + 1) + ')').attr('href', leftAbsPath);
 			$('ul.paintings.left-side a:nth-child(' + (i + 1) + ') li').css('background', 'linear-gradient(0deg, rgba(0,0,0,0), rgba(0,0,0,0)), url(' + leftAbsPath + ') no-repeat');
-			$('ul.paintings.left-side a:nth-child(' + (i + 1) + ') li').mouseover(function() {
-				$(this).css('background', 'linear-gradient(0deg, rgba(0,0,0,0.7), rgba(0,0,0,0.7)), url(' + leftAbsPath + ') no-repeat');
-			});
-			$('ul.paintings.left-side a:nth-child(' + (i + 1) + ') li').mouseout(function() {
-				$(this).css('background', 'linear-gradient(0deg, rgba(0,0,0,0.0), rgba(0,0,0,0.0)), url(' + leftAbsPath + ') no-repeat');
-			});
 
 
 			$('ul.paintings.right-side a:nth-child(' + (i + 1) + ') span.title').text(rightImgTitle);
