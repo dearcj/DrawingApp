edc7acbea21e9fa35bfd3a6ad80003312fdc2fb4
Alexey <varenicza@gmail.com>
2016-05-09 06:57:38 +0300

    routing

--- js/scripts.js
+++ js/scripts.js
@@ -750,55 +750,55 @@ jQuery(document).ready(function($) {
 	// GENERIC PAGES
 
 	$('#button-1').on(window.eventType, function(event) {
-		$('#index-wrapper').fadeOut(10).addClass('hidden');
 		$('#installation-wrapper').fadeIn(1000).removeClass('hidden').animate({'opacity': 1}, 200);
+		$('#index-wrapper').fadeOut(10).addClass('hidden');
 		openPage('installation');
 	});
 
 	$('#button-2').on(window.eventType, function(event) {
-		$('#index-wrapper').fadeOut(10).addClass('hidden');
 		$('#performance-wrapper').fadeIn(1000).removeClass('hidden').animate({'opacity': 1}, 200);
+		$('#index-wrapper').fadeOut(10).addClass('hidden');
 		openPage('performance');
 	});
 
 	$('#button-5').on(window.eventType, function(event) {
-		$('#index-wrapper').fadeOut(10).addClass('hidden');
 		$('#interpretation-wrapper').fadeIn(1000).removeClass('hidden').animate({'opacity': 1}, 200);
+		$('#index-wrapper').fadeOut(10).addClass('hidden');
 		openPage('interpretation');
 	});
 
 	$('#button-6').on(window.eventType, function(event) {
-		$('#index-wrapper').fadeOut(10).addClass('hidden');
 		$('#mediaart-wrapper').fadeIn(1000).removeClass('hidden').animate({'opacity': 1}, 200);
+		$('#index-wrapper').fadeOut(10).addClass('hidden');
 		openPage('media-art');
 	});
 
 	$('li.installation').on(window.eventType, function(event) {
-		$('#mediaart-wrapper').fadeOut(10).addClass('hidden');
-		$('#performance-wrapper').fadeOut(50).addClass('hidden');
 		$('#installation-wrapper').fadeIn(1000).removeClass('hidden').animate({'opacity': 1}, 200);
+		$('#mediaart-wrapper').fadeOut(10).addClass('hidden');
+		$('#performance-wrapper').fadeOut(10).addClass('hidden');
 		openPage('installation');
 	});
 
 	$('li.performance').on(window.eventType, function(event) {
+		$('#performance-wrapper').fadeIn(1000).removeClass('hidden').animate({'opacity': 1}, 200);
 		$('#installation-wrapper').fadeOut(10).addClass('hidden');
 		$('#interpretation-wrapper').fadeOut(10).addClass('hidden');
-		$('#performance-wrapper').fadeIn(1000).removeClass('hidden').animate({'opacity': 1}, 200);
 		openPage('performance');
 	});
 
 
 	$('li.interpretation').on(window.eventType, function(event) {
+		$('#interpretation-wrapper').fadeIn(1000).removeClass('hidden').animate({'opacity': 1}, 200);
 		$('#performance-wrapper').fadeOut(10).addClass('hidden');
 		$('#mediaart-wrapper').fadeOut(10).addClass('hidden');
-		$('#interpretation-wrapper').fadeIn(1000).removeClass('hidden').animate({'opacity': 1}, 200);
 		openPage('interpretation');
 	});
 
 	$('li.media-art').on(window.eventType, function(event) {
+		$('#mediaart-wrapper').fadeIn(1000).removeClass('hidden').animate({'opacity': 1}, 200);
 		$('#interpretation-wrapper').fadeOut(10).addClass('hidden');
 		$('#installation-wrapper').fadeOut(10).addClass('hidden');
-		$('#mediaart-wrapper').fadeIn(1000).removeClass('hidden').animate({'opacity': 1}, 200);
 		openPage('media-art');
 	});
 
