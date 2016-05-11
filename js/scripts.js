ec9331536359c9cc55319bba7dca6d0992881d61
Alexey <varenicza@gmail.com>
2016-05-11 16:46:52 +0300

    routing

--- js/scripts.js
+++ js/scripts.js
@@ -539,7 +539,7 @@ jQuery(document).ready(function($) {
 		$('ul.surfaces').animate({'margin-left': -$(window).width()/5.28 + 'px'}, 500);
 		$('.go-to-tools').animate({'top': 150 + 'px'}, 500);
 		$('#surface-wrapper').delay(500).animate({'opacity': 0}, 100).css('z-index', '100');
-		$('#filters-wrapper').delay(500).animate({'opacity': 1}, 100).css('z-index', '100');
+		$('#filters-wrapper').delay(500).animate({'opacity': 0}, 100).css('z-index', '100');
 		$('#surface-wrapper').each(function(){
 			$(this).children().css({'z-index': '100', 'opacity': 0});
 		});
@@ -587,7 +587,7 @@ jQuery(document).ready(function($) {
 	}
 
 	function filtersAnimation() {
-		$('#painting-wrapper').delay(500).animate({'opacity': 0}, 100).css('z-index', '100');
+		$('#painting-wrapper').animate({'opacity': 0}, 100).css('z-index', '100');
 		$('#painting-wrapper').each(function(){
 			$(this).children().css({'z-index': '100', 'opacity': 0});
 		});
