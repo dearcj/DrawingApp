2131c2bad8bc8d501d5d07e38a433731a75240df
Alexey <varenicza@gmail.com>
2016-05-12 16:02:03 +0300

    routing

--- js/scripts.js
+++ js/scripts.js
@@ -1120,7 +1120,7 @@ jQuery(document).ready(function($) {
 	function openMyMuseum() {
 		window.imageId = localStorage.getItem('imageId');
 		getLastImage(imageId, function (res) {
-			var message  = res.name + ': ' + res.description + " Я экспериментирую вместе с Московским музеем современного искусства. Заходи в мастерскую PLAYMMOMA, используй необычные инструменты и создай свою картину! "  + " http://play.mmoma.ru/";
+			var message  = res.name + ': «' + res.description + "» Я экспериментирую вместе с Московским музеем современного искусства. Заходи в мастерскую #PLAYMMOMA, используй необычные инструменты и создай свою картину! "  + " http://play.mmoma.ru/";
 			document.getElementById('post-vk').addEventListener('click', function() {
 				vkpost(message, res.image, res.file);
 			});
