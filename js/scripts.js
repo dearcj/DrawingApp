492d037f76aee3792ea4a69883928df209acb17c
Alexey <varenicza@gmail.com>
2016-05-12 14:07:20 +0300

    routing

--- js/scripts.js
+++ js/scripts.js
@@ -60,7 +60,7 @@ jQuery(document).ready(function($) {
 	});
 
 	$('.interact').mouseover(function() {
-		setTimeout("ZSound.Play('interact');", 1000);
+		setTimeout("ZSound.Play('interact');", 500);
 	});
 
 	$('ul.surfaces li').on(window.eventType, function(event) {
