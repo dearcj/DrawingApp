eb8563d7de54a824ff0e59e9de6711434d233163
Alexey <varenicza@gmail.com>
2016-05-14 21:22:37 +0300

    routing

--- js/scripts.js
+++ js/scripts.js
@@ -432,16 +432,18 @@ jQuery(document).ready(function($) {
 		$('ul.paintings.left-side a:nth-child(1)').colorbox({title: function() {
 			var author = $(this).find("span.author").text();
 			var title = $(this).find("span.title").text();
-			var desc = $(this).find("span.materials").text();
-			return author + '. ' + title + ' ' + desc;
+			var mats = $(this).find("span.materials").text();
+			var desc = $(this).find("span.description").text();
+			return author + '. ' + title + ' ' + mats + desc;
 		}
 		});
 
 		$('ul.paintings.left-side a:nth-child(2)').colorbox({title: function() {
 			var author = $(this).find("span.author").text();
 			var title = $(this).find("span.title").text();
-			var desc = $(this).find("span.materials").text();
-			return author + '. ' + title + ' ' + desc;
+			var mats = $(this).find("span.materials").text();
+			var desc = $(this).find("span.description").text();
+			return author + '. ' + title + ' ' + mats + desc;
 		}
 		});
 
