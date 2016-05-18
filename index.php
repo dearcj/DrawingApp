eb6539c345da46fc004f57dc28d5e2fc943097b3
Alexey <varenicza@gmail.com>
2016-05-18 21:21:12 +0300

    routing

--- index.php
+++ index.php
@@ -412,7 +412,7 @@
               </ul>
             <span id="save">Сохранить</span>
               <ul>
-                <li id="download"></li>
+                <li id="download"><a href="http://play.mmoma.ru/onlinegame/wp-content/themes/mmoma/api/' + res.file" download></a></li>
                 <li id="mail-send"></li>
               </ul>
           </div>
