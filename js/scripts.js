754250984d6fec3e7ecc1c878614190ca01ad5f8
KURWINDALLAS <ddearcj@gmail.com>
2016-05-25 20:11:19 +0300

    ico

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,26 @@ window.basePath = './wp-content/themes/mmoma/';
 
 window.selectedSurface = 1;
 
-ZSound.Init([
+
+function msieversion() {
+
+	var ua = window.navigator.userAgent;
+	var msie = ua.indexOf("MSIE ");
+
+	if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))  // If Internet Explorer, return version number
+	{
+		alert(parseInt(ua.substring(msie + 5, ua.indexOf(".", msie))));
+	}
+	else  // If another browser, return 0
+	{
+		alert('otherbrowser');
+	}
+
+	return false;
+}
+
+
+/*ZSound.Init([
 	{id: 'scotch', src: "scotch.ogg"},
 	{id: 'click', src: "click.ogg"},
 	{id: 'filter', src: "filter.ogg"},
@@ -20,6 +39,7 @@ ZSound.Init([
 	{id: 'needle', src: "needle.ogg"},
 	{id: 'orange', src: "orange.ogg"},
 ]);
+*/
 window.eventType = $.support.touch ? "tap" : "click";
 
 ZSound.soundLoadedFunction = function() {
