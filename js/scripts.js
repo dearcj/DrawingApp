ffeb67ec723580bf0c71b5fbf9b9a2a541cd55fb
KURWINDALLAS <ddearcj@gmail.com>
2016-05-25 20:13:37 +0300

    ico

--- js/scripts.js
+++ js/scripts.js
@@ -12,14 +12,12 @@ function msieversion() {
 
 	if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))  // If Internet Explorer, return version number
 	{
-		alert(parseInt(ua.substring(msie + 5, ua.indexOf(".", msie))));
+		return true;
 	}
 	else  // If another browser, return 0
 	{
-		alert('otherbrowser');
+		return false;
 	}
-
-	return false;
 }
 
 if (msieversion() == false) {
