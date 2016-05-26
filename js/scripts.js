f450fc0340972e873030e2d68969e7e4d7589405
Alexey <varenicza@gmail.com>
2016-05-26 13:28:20 +0300

    mobile

--- js/scripts.js
+++ js/scripts.js
@@ -458,7 +458,8 @@ jQuery(document).ready(function($) {
 
 			$('ul.paintings.left-side a:nth-child(' + (i + 1) + ') li').mouseover(function() {
 				var thumb = $(this).css('background');
-				console.log(thumb);
+				var thumbHover = thumb.replace('rgba(0,0,0,0)', 'rgba(0,0,0,0.4)');
+				$(this).css('background', thumbHover);
 			});
 		}
 
