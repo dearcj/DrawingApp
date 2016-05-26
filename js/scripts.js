23d1c2132bd68763886449b3ecd2a5bfb4d44a46
Alexey <varenicza@gmail.com>
2016-05-26 05:10:10 +0300

    routing

--- js/scripts.js
+++ js/scripts.js
@@ -578,6 +578,7 @@ jQuery(document).ready(function($) {
 		var surfaceWidth = parseInt($('ul.surfaces li').css('width'));
 		var surfaceHeight = parseInt($('ul.surfaces li').css('height'));
 		var canvasHeight = parseInt($('#surface-canvas').css('height'));
+		var canvasTop = parseInt($('#surface-canvas').css('margin-top'));
 
 		if ($(window).width() < 700) {
 			$('#surface-wrapper').fadeIn(300).removeClass('hidden').animate({'opacity': 1, 'height': 100 + '%'}, 200).css('z-index', '200');
@@ -587,7 +588,7 @@ jQuery(document).ready(function($) {
 			$('#go-to-tools').delay(1000).animate({'top': 0 + 'px'}, 500);
 		}
 
-		$('ul.surfaces').css('margin-top', (canvasHeight - surfaceHeight*4)/2 + 'px').delay(500).animate({'left': 50 + '%'}, 1500);
+		$('ul.surfaces').css('margin-top', (canvasHeight - surfaceHeight*4)/2 + canvasTop + 'px').delay(500).animate({'left': 50 + '%'}, 1500);
 
 		$('.surface-button').css({width: surfaceWidth/1.216 + 'px', height: surfaceHeight/1.469 + 'px'}).delay(1500).animate({'left': $(window).width()/100 + 'px'}, 500);
 
