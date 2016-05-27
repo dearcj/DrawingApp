23253e7be865f54755ac6c4dffe35685897f2923
Alexey <varenicza@gmail.com>
2016-05-27 23:19:49 +0300

    mobile

--- js/scripts.js
+++ js/scripts.js
@@ -675,7 +675,7 @@ jQuery(document).ready(function($) {
 			$('#museum-wrapper > p').css({width: 145 + 'px', 'font-size': 100 + '%', 'padding': '18px 18px 22px', 'border-radius': 12 + 'px', top: -225 + 'px'});
 		} else {
 			$('#museum-wrapper').css('display', 'block').animate({'opacity': 1}, 1000).css('z-index', '200');
-			$('.new-painting').css({width: 150 + 'px', height: 50 + 'px'}).delay(500).animate({'top': -120 + 'px'}, 1000);
+			$('.new-painting').css({width: 150 + 'px', height: 50 + 'px'}).delay(500).animate({'top': 300 + 'px'}, 1000);
 			$('#museum-wrapper > p').css({width: 300 + 'px', 'font-size': 200 + '%', 'padding': '20px 20px 25px', 'border-radius': 20 + 'px', top: -285 + 'px'});
 			$('.first-exp-canvas').delay(1000).animate({top: 25 + 'px'}, 1000);
 		}
