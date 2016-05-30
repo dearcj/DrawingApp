c3df16a6864749e697c32e06045736e73c86f32e
Alexey <varenicza@gmail.com>
2016-05-30 07:29:36 +0300

    mobile

--- js/scripts.js
+++ js/scripts.js
@@ -19,8 +19,9 @@ function msieversion() {
 	}
 }
 
-if ( $.browser.safari ) {
-	console.log('safari');
+if ( $.browser.webkit ) {
+	$('#post-vk').css('display', 'none');
+	$('#post-fb').css({'float': 'none', 'margin': '0 auto'});
 }
 
 if (msieversion() == false) {
@@ -48,6 +49,7 @@ ZSound.soundLoadedFunction = function() {
 	ZSound.PlayMusic('street');
 }
 
+jQuery(window).load(function($) {
 	$('#wrapper').animate({'opacity': 1}, 4000);
 
 	$('#mute').on(window.eventType, function(event) {
@@ -1560,3 +1562,4 @@ ZSound.soundLoadedFunction = function() {
 	helpersTextProperties();
 	buttonHelpersPosition();
 	coordinateHelpers();
+});
