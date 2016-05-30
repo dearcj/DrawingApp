32f92286f2bbfbdd827dda1e19b35bbc4c75b1e9
Alexey <varenicza@gmail.com>
2016-05-30 09:27:54 +0300

    mobile

--- js/scripts.js
+++ js/scripts.js
@@ -1449,9 +1449,9 @@ jQuery(document).ready(function($) {
 		$('ul#helpers li#helper-5 span.up').css({left: -spanWidth/coeff + 'px', top: spanTopPos * 5.485 + 'px'});
 		$('ul#helpers li#helper-6 span').css({top: -spanTopPos + 'px', width: width/100 * 15 + 'px'});
 		$('ul#helpers li#helper-6s span').css({top: -spanTopPos + 'px', width: width/100 * 15 + 'px'});
-		$('ul#helpers li#helper-7 span').css({left: -spanWidth/coeff + 'px', top: -spanTopPos * 0.9 + 'px'});
-		$('ul#helpers li#helper-8 span').css({left: -width/200 * 20 + 'px', top: -spanTopPos * 0.9 + 'px', width: width/100 * 20 + 'px'});
-		$('ul#helpers li#helper-9 span').css({left: -width/200 * 15 + 'px', top: spanTopPos * 2 + 'px', width: width/100 * 15 + 'px'});
+		$('ul#helpers li#helper-7 span').css({left: -spanWidth/2.264 + 'px', top: -spanTopPos * 0.9525 + 'px'});
+		$('ul#helpers li#helper-8 span').css({left: -spanWidth/coeff + 'px', top: -spanTopPos * 0.9525 + 'px', width: width/100 * 20 + 'px'});
+		$('ul#helpers li#helper-9 span').css({left: -spanWidth/coeff + 'px', top: spanTopPos * 2 + 'px', width: width/100 * 15 + 'px'});
 		$('ul#helpers li#helper-10 span').css({left: -spanWidth/coeff + 'px', top: spanTopPos * 2.7 + 'px'});
 		$('ul#helpers li#helper-11 span').css({left: -spanWidth/coeff + 'px', top: -spanTopPos * 0.9 + 'px'});
 		$('ul#helpers li#helper-12 span').css({left: -spanWidth/coeff + 'px', top: -spanTopPos * 0.9 + 'px'});
