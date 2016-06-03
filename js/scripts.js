3ce80955a1793dfa7b76b77fbe7e5161031101e1
Alexey <varenicza@gmail.com>
2016-06-03 18:46:21 +0300

    mobile

--- js/scripts.js
+++ js/scripts.js
@@ -403,8 +403,8 @@ jQuery(document).ready(function($) {
 				rightImgTitle = imgs[j-i].title;
 				rightImgAuthor = imgs[j-i].author;
 				rightImgMats = imgs[j-i].materials;
-				rightImgDesc = imgs[j-1].description;
-				rightImgYear = imgs[j-1].year;
+				rightImgDesc = imgs[j-i].description;
+				rightImgYear = imgs[j-i].year;
 
 				$('ul.paintings.right-side a:nth-child(' + (i + 1) + ') span.title').text(rightImgTitle);
 				$('ul.paintings.right-side a:nth-child(' + (i + 1) + ') span.author').text(rightImgAuthor);
